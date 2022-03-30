cd8b5646f40d96ed84608ee0dfc494b329809ffa
tmserl <tom.earl@hey.com>
2022-03-30 20:02:13 +0100

    Components created

---
+++ src/components/NoteFlow/NoteDate/index.tsx
@@ -0,0 +1,5 @@
+function NoteDate({ date }: { date: string }) {
+  return <h2 className="notes-date">{date}</h2>;
+}
+
+export default NoteDate;
--- src/components/NoteFlow/NoteStream/index.tsx
+++ src/components/NoteFlow/NoteStream/index.tsx
@@ -1,9 +1,14 @@
+import NoteDate from '../NoteDate';
 import NoteCard from '../NoteCard';
 
 function NotesStream() {
   return (
     <div className="notes-stream column">
+      <NoteDate date="Today" />
       <NoteCard content="This is another note after that!." time="16:16" />
+      <NoteCard content="This is another note after that!." time="16:16" />
+      <NoteDate date="Yesterday" />
+      <NoteCard content="This is my first note." time="16:15" />
       <NoteCard content="This is my first note." time="16:15" />
     </div>
   );
