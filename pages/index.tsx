8a5689297c9a14822c45b9706d75e43b63148dae
tmserl <tom.earl@hey.com>
2022-04-02 17:13:04 +0100

    New note logic with Supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import NoteFlow from '../src/components/NoteFlow';
 import supabase from '../utils/supabaseClient';
 
 function Home() {
-  console.log(supabase.auth.user());
-
   return (
     <div className="wrapper">
       <section className="noteflow">
--- src/components/NoteFlow/index.tsx
+++ src/components/NoteFlow/index.tsx
@@ -5,8 +5,12 @@ import NotesStream from './NoteStream';
 
 // FIXME: Statically type notesData state
 // FIXME: Statically type event parameter
+// FIXME: Statically type user object
+// TODO: Refactor code for fetching all notes and creating new notes
 
 function NoteFlow() {
+  const user: any = supabase.auth.user();
+
   // 'Create note' Button
   const [createNoteBtnToggle, setCreateNoteBtnToggle] =
     useState<boolean>(false);
@@ -25,13 +29,31 @@ function NoteFlow() {
   // Notes data from Supabase
   const [notesData, setNotesData] = useState<any>();
 
+  // Fetch all notes
+  async function fetchNotes() {
+    const { data: notes } = await supabase
+      .from('notes')
+      .select('*')
+      .order('created_at', { ascending: false });
+    setNotesData(notes);
+  }
+
+  // Fetch notes on page mount
   useEffect(() => {
-    async function fetchNotes() {
-      const { data: notes } = await supabase
+    fetchNotes();
+  }, []);
+
+  // Create new note
+  useEffect(() => {
+    async function newNote() {
+      const { data, error } = await supabase
         .from('notes')
-        .select('*')
-        .order('created_at', { ascending: false });
-      setNotesData(notes);
+        .insert([{ note_content: noteCreatorContent, user_id: user.id }]);
+    }
+    if (user) {
+      if (noteCreatorContent.length >= 1) {
+        newNote();
+      }
     }
     fetchNotes();
   }, [createNoteBtnToggle]);
