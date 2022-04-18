e18ebdbf8d0d2d5d1d5313cbadb2a568b27deda3
tmserl <tom.earl@hey.com>
2022-04-18 17:13:41 +0100

    Created function to sort array by date. Mapping through the object.

--- src/components/NoteFlow/NoteStream/index.tsx
+++ src/components/NoteFlow/NoteStream/index.tsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import NoteCard from './NoteCard';
+import NoteDate from './NoteDate';
 
 // FIXME: Statically type props
 
@@ -9,10 +11,48 @@ function NotesStream({
   notesData: any;
   deleteNote: any;
 }) {
+  const [groupedNotesByDate, setGroupedNotesByDate] = useState<object>({});
+
+  useEffect(() => {
+    if (notesData) {
+      const groupByDate = (noteData: any) => {
+        return noteData.reduce((groups: any, note: any) => {
+          const date = new Date(note.created_at).toLocaleDateString('en-GB', {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+          });
+          if (!groups.hasOwnProperty(date)) {
+            groups[date] = [];
+          }
+          groups[date].push(note);
+          return groups;
+        }, {});
+      };
+      setGroupedNotesByDate(groupByDate(notesData));
+    }
+  }, []);
+
+  console.log(groupedNotesByDate);
+
   return (
     <div className="notes-stream column">
       {notesData && (
         <>
+          {Object.entries(groupedNotesByDate).map(([date, notes], i) => (
+            <>
+              <NoteDate date={date} />
+              {notes.map((note: any) => (
+                <NoteCard
+                  key={note.id}
+                  id={note.id}
+                  content={note.note_content}
+                  time={note.created_at}
+                  deleteNote={deleteNote}
+                />
+              ))}
+            </>
+          ))}
           {notesData.map((note: any) => (
             <NoteCard
               key={note.id}
@@ -24,7 +64,6 @@ function NotesStream({
           ))}
         </>
       )}
-      {/* <NoteDate date="Today" />*/}
     </div>
   );
 }
