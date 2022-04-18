95930a36a9340b7ac4c68fda88995fd6d630a274
tmserl <tom.earl@hey.com>
2022-04-18 17:16:55 +0100

    Removed duplicated component from the bottom

--- src/components/NoteFlow/NoteStream/index.tsx
+++ src/components/NoteFlow/NoteStream/index.tsx
@@ -33,8 +33,6 @@ function NotesStream({
     }
   }, []);
 
-  console.log(groupedNotesByDate);
-
   return (
     <div className="notes-stream column">
       {notesData && (
@@ -52,15 +50,6 @@ function NotesStream({
                 />
               ))}
             </>
-          ))}
-          {notesData.map((note: any) => (
-            <NoteCard
-              key={note.id}
-              id={note.id}
-              content={note.note_content}
-              time={note.created_at}
-              deleteNote={deleteNote}
-            />
           ))}
         </>
       )}
