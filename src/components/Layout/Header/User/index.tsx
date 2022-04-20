dceb55bf8a4c3ee4b7803dedd0bb83edf570ddef
tmserl <tom.earl@hey.com>
2022-04-20 20:30:31 +0100

    Added logic to blank user icon if not logged in

--- src/components/Layout/Header/User/index.tsx
+++ src/components/Layout/Header/User/index.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import supabase from '../../../../../utils/supabaseClient';
 
 function User() {
+  const user: any = supabase.auth.user();
   const [isAvatarClicked, setIsAvatarClicked] = useState<boolean>(false);
 
   // Sets isAvatarClicked to false on page mount
@@ -13,10 +15,31 @@ function User() {
     setIsAvatarClicked(!isAvatarClicked);
   }
 
+  // Gets name initials from authenticated users
+  const [userInitials, setUserInitials] = useState<string>('');
+
+  useEffect(() => {
+    function getInitialsFromUserFullName(fullName: string) {
+      const allNames = fullName.trim().split(' ');
+      const initials = allNames.reduce((acc, curr, index) => {
+        if (index === 0 || index === allNames.length - 1) {
+          acc = `${acc}${curr.charAt(0).toUpperCase()}`;
+        }
+        return acc;
+      }, '');
+      return initials;
+    }
+    if (user) {
+      setUserInitials(getInitialsFromUserFullName(user.user_metadata.name));
+    } else {
+      setUserInitials('');
+    }
+  }, []);
+
   return (
     <>
       <div onClick={handleAvatarClick} className="header--user-icon">
-        <p className="header--user-initials">TE</p>
+        <p className="header--user-initials">{userInitials}</p>
       </div>
       {isAvatarClicked && (
         <div className="header--user-login">
