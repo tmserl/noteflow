a09213d939ac3cacc2f81a87107a48541b653b27
tmserl <tom.earl@hey.com>
2022-04-25 18:19:30 +0100

    Added loading animation to timeline-line whilst authenticating

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { motion } from 'framer-motion';
 import supabase from '../utils/supabaseClient';
 
 function Login() {
@@ -6,7 +7,32 @@ function Login() {
     supabase.auth.signIn({ provider: 'github' });
   }, []);
 
-  return <p>Logging in</p>;
+  return (
+    <div className="wrapper">
+      <section className="noteflow">
+        <motion.div
+          animate={{
+            backgroundImage: [
+              'linear-gradient(#ebebeb 0%,#d8d8d8 20%,#ebebeb 40%)',
+              'linear-gradient(#ebebeb 20%,#d8d8d8 40%,#ebebeb 60%)',
+              'linear-gradient(#ebebeb 40%,#d8d8d8 60%,#ebebeb 80%)',
+              'linear-gradient(#ebebeb 60%,#d8d8d8 80%,#ebebeb 100%)',
+              'linear-gradient(#ebebeb 80%,#d8d8d8 100%,#ebebeb 20%)',
+              'linear-gradient(#ebebeb 100%,#d8d8d8 20%,#ebebeb 40%)',
+            ],
+          }}
+          transition={{ duration: 0.5, repeat: Infinity }}
+          className="column timeline-line"
+        >
+          <div className="note-card note-card__center">
+            <textarea placeholder="Write your note"></textarea>
+            <button>Create note</button>
+          </div>
+          <div className="spacer"></div>
+        </motion.div>
+      </section>
+    </div>
+  );
 }
 
 export default Login;
