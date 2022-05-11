feb70cb3ac1e710dda429759d449ad0e563b4bbe
tmserl <tom.earl@hey.com>
2022-05-11 15:55:35 +0100

    Temporarily typed as "any"

--- src/components/NoteFlow/NoteStream/index.tsx
+++ src/components/NoteFlow/NoteStream/index.tsx
@@ -35,29 +35,33 @@ function NotesStream({
       )}
       {sortedNotesData && (
         <>
-          {Object.entries(sortedNotesData).map(([noteDate, noteContent], i) => (
-            <div key={i}>
-              <NoteDate noteDate={noteDate} />
-              <div className="column">
-                <AnimatePresence>
-                  {noteContent
-                    .filter((item) => categoryToggles.includes(item.category))
-                    .map((note: any) => (
-                      <>
-                        <NoteCard
-                          key={note.id}
-                          id={note.id}
-                          content={note.note_content}
-                          category={note.category}
-                          time={note.created_at}
-                          deleteNote={deleteNote}
-                        />
-                      </>
-                    ))}
-                </AnimatePresence>
+          {Object.entries(sortedNotesData).map(
+            ([noteDate, noteContent]: [noteDate: any, noteContent: any], i) => (
+              <div key={i}>
+                <NoteDate noteDate={noteDate} />
+                <div className="column">
+                  <AnimatePresence>
+                    {noteContent
+                      .filter((item: any) =>
+                        categoryToggles.includes(item.category)
+                      )
+                      .map((note: any) => (
+                        <>
+                          <NoteCard
+                            key={note.id}
+                            id={note.id}
+                            content={note.note_content}
+                            category={note.category}
+                            time={note.created_at}
+                            deleteNote={deleteNote}
+                          />
+                        </>
+                      ))}
+                  </AnimatePresence>
+                </div>
               </div>
-            </div>
-          ))}
+            )
+          )}
         </>
       )}
     </div>
